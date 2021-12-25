43a2bcdb64b2c3136facaab1df40098feedece09
unknown <haritsah@student.telkomuniversity.ac.id>
2021-12-25 11:14:26 +0700

    PROGRESS MINGGU 4

--- COFEE-ROASTRY/resources/views/layout.blade.php
+++ COFEE-ROASTRY/resources/views/layout.blade.php
@@ -12,32 +12,34 @@
 
     </head>
 <body>
-<nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top">
+<nav class="navbar navbar-expand-lg navbar-dark bg-success fixed-top">
     <div class="container">
-        <a class="navbar-brand" href="#"><img src="https://i.ibb.co/n7zW8r4/30e1d5ea-e6f5-4f89-8d26-fade422acd40.jpg" alt="Logo" height="30px" width="40px" class="img-fluid" /></a>
+        <a class="navbar-brand" href="#"><img src="https://i.ibb.co/Z2c6KpC/Whats-App-Image-2021-12-23-at-11-49-53-removebg-preview-2.png" alt="Logo" height="60px" width="90px" class="img-fluid" /></a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarNavDropdown">
             <ul class="navbar-nav mx-auto">
                 <li class="nav-item">
-                    <a class="nav-link active" href="#">Home</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="{{ url('/product')}}">Product</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="{{ url('/aboutus')}}">About Us</a>
+                    <a class="nav-link" href="#">Home</a>
                 </li>
                 <li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                        View More
+                        Product
                     </a>
                     <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                        <li><a class="dropdown-item" href="#">Web Development</a></li>
-                        <li><a class="dropdown-item" href="#">Web Designing</a></li>
-                        <li><a class="dropdown-item" href="#">Android Development</a></li>
+                        <li><a class="dropdown-item" href="{{ url('/product')}}">Product Items</a></li>
+                        <li><a class="dropdown-item" href="#">Product Reviews</a></li>
+                        <li><a class="dropdown-item" href="#">Product Chart</a></li>
+                        <li><a class="dropdown-item" href="#">Product Order</a></li>
+                        <li><a class="dropdown-item" href="#">Product Status</a></li>
                     </ul>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ url('/gallery')}}">Gallery</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ url('/aboutus')}}">About Us</a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="#">Contact Us</a>
@@ -49,7 +51,7 @@
                 </form>
                 <h5><i class="fas fa-shopping-cart text-light m-2"></i></h5>
                 <h5><i class="fas fa-bell text-light m-2"></i></i></h5>
-                <a href="{{ url('/login')}}"><button class="btn btn-light ms-3">Login</button></a>
+                <a href="{{ url('/login')}}"><h5><i class="fas fa-sign-in-alt text-light m-2"> Login</i></h5></a>
             </div>
         </div>
     </div>
--- COFEE-ROASTRY/resources/views/product.blade.php
+++ COFEE-ROASTRY/resources/views/product.blade.php
@@ -2,48 +2,114 @@
 @section('judul','Home')
 @section('section')
 
-<div class="container mt-100">
-    <div class="row">
-        <div class="col-md-4 col-sm-6">
-            <div class="card mb-30"><a class="card-img-tiles" href="#" data-abc="true">
-                    <div class="inner">
-                        <div class="main-img"><img src="https://i.imgur.com/O0GMYuw.jpg" alt="Category"></div>
-                        <div class="thumblist"><img src="https://i.imgur.com/ILEU18M.jpg" alt="Category"><img src="https://i.imgur.com/2kePJmX.jpg" alt="Category"></div>
+<div class="row">
+    <div class="col-md-2">
+    <ul class="list-group">
+        <li class="list-group-item">Semua produk</li>
+        <li class="list-group-item">Discount</li>
+        <li class="list-group-item">Kategori</li>
+        <li class="list-group-item">Beans</li>
+        <li class="list-group-item">Powder</li>
+        <li class="list-group-item">Tools</li>
+        <li class="list-group-item">Urutan</li>
+        <li class="list-group-item">Terpopuler</li>
+        <li class="list-group-item">Terbaru</li>
+        <li class="list-group-item">Terlama</li>
+        <li class="list-group-item">Nama Produk</li>
+    </ul>
+    </div>
+    <div class="col-md-10">
+        <div class="row">
+            <div class="col-md-4 col-sm-6">
+                <div class="card mb-30"><a class="card-img-tiles" href="#" data-abc="true">
+                        
+                            <div class="main-img"><img src="https://images.tokopedia.net/img/cache/400/WjdAsz/2021/8/28/30fb412c-8e35-4825-bd9d-fe58c79130eb.jpg?ect=4g" alt="Category"></div>
+                    </a>
+                    <div class="card-body text-center">
+                        <h4 class="card-title">Cofee Tubruk</h4>
+                        <p class="text-muted">Starting from $499</p><a class="btn btn-outline-success btn-sm m-2" href="#" data-abc="true">Buy Products</a><a class="btn btn-outline-primary btn-sm m-2" href="#" data-abc="true" data-bs-toggle="modal" data-bs-target="#produk1">Detail</a>
                     </div>
-                </a>
-                <div class="card-body text-center">
-                    <h4 class="card-title">Laptops</h4>
-                    <p class="text-muted">Starting from $499</p><a class="btn btn-outline-primary btn-sm" href="#" data-abc="true">Buy Products</a>
                 </div>
             </div>
-        </div>
-        <div class="col-md-4 col-sm-6">
-            <div class="card mb-30"><a class="card-img-tiles" href="#" data-abc="true">
-                    <div class="inner">
-                        <div class="main-img"><img src="https://i.imgur.com/uRgdVY1.jpg" alt="Category"></div>
-                        <div class="thumblist"><img src="https://i.imgur.com/VwSKS7A.jpg" alt="Category"><img src="https://i.imgur.com/gTvZ2H5.jpg" alt="Category"></div>
+            
+            <div class="modal fade" id="produk1" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div class="modal-dialog modal-lg">
+                    <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                     </div>
-                </a>
-                <div class="card-body text-center">
-                    <h4 class="card-title">Mobiles</h4>
-                    <p class="text-muted">Starting from $50</p><a class="btn btn-outline-primary btn-sm" href="#" data-abc="true">Buy Products</a>
+                    <div class="modal-body">
+                        <div class="row">
+                            <div class="col-md-6">
+                            <img src="https://images.tokopedia.net/img/cache/400/WjdAsz/2021/8/28/30fb412c-8e35-4825-bd9d-fe58c79130eb.jpg?ect=4g" alt="Category">
+                            </div>
+                            <div class="col-md-6">
+                                <table class="table table-borderless m-3">
+                                    <tr>
+                                        <th>Nama Produk</th>
+                                        <td>Cofee Tubruk Robusta</td>
+                                    </tr>
+                                    <tr>
+                                        <th>Merk</th>
+                                        <td>D'Colle</td>
+                                    </tr>
+                                    <tr>
+                                        <th>Stok Produk</th>
+                                        <td>120 Pcs</td>
+                                    </tr>
+                                    <tr>
+                                        <th>Harga</th>
+                                        <td>Rp. 300.000</td>
+                                    </tr>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                        <button type="button" class="btn btn-primary">Buy Products</button>
+                    </div>
+                    </div>
                 </div>
             </div>
-        </div>
-        <div class="col-md-4 col-sm-6">
-            <div class="card mb-30"><a class="card-img-tiles" href="#" data-abc="true">
-                    <div class="inner">
-                        <div class="main-img"><img src="https://i.imgur.com/0jO40CF.jpg" alt="Category"></div>
-                        <div class="thumblist"><img src="https://i.imgur.com/dWYAg41.jpg" alt="Category"><img src="https://i.imgur.com/5oQEZSC.jpg" alt="Category"></div>
+
+            <div class="col-md-4 col-sm-6">
+                <div class="card mb-30"><a class="card-img-tiles" href="#" data-abc="true">
+                            <div class="main-img"><img src="https://cf.shopee.co.id/file/006705ae05d0425320bda4b5caedc5c7" alt="Category"></div>
+                    </a>
+                    <div class="card-body text-center">
+                        <h4 class="card-title">Cofee Mix</h4>
+                        <p class="text-muted">Starting from $50</p><a class="btn btn-outline-success btn-sm m-2" href="#" data-abc="true">Buy Products</a><a class="btn btn-outline-primary btn-sm m-2" href="#" data-abc="true">Detail</a>
                     </div>
-                </a>
-                <div class="card-body text-center">
-                    <h4 class="card-title">Accessories</h4>
-                    <p class="text-muted">Starting from $9</p><a class="btn btn-outline-primary btn-sm" href="#" data-abc="true">Buy Products</a>
+                </div>
+            </div>
+            <div class="col-md-4 col-sm-6">
+                <div class="card mb-30"><a class="card-img-tiles" href="#" data-abc="true">
+                            <div class="main-img"><img src="https://images.tokopedia.net/img/cache/500-square/attachment/2020/9/17/66236640/66236640_08a0a2e6-f16d-485b-8669-f88eccb7e280.jpg" alt="Category"></div>
+                    </a>
+                    <div class="card-body text-center">
+                        <h4 class="card-title">Cofee Arabika</h4>
+                        <p class="text-muted">Starting from $9</p><a class="btn btn-outline-success btn-sm m-2" href="#" data-abc="true">Buy Products</a><a class="btn btn-outline-primary btn-sm m-2" href="#" data-abc="true">Detail</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4 col-sm-6">
+                <div class="card mb-30"><a class="card-img-tiles" href="#" data-abc="true">
+                            <div class="main-img"><img src="https://cf.shopee.co.id/file/006705ae05d0425320bda4b5caedc5c7" alt="Category"></div>
+                    </a>
+                    <div class="card-body text-center">
+                        <h4 class="card-title">Cofee Mix</h4>
+                        <p class="text-muted">Starting from $50</p><a class="btn btn-outline-success btn-sm m-2" href="#" data-abc="true">Buy Products</a><a class="btn btn-outline-primary btn-sm m-2" href="#" data-abc="true">Detail</a>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
+</div>
+
+<div class="container mt-100">
+    
 </div>
 
 @endsection
